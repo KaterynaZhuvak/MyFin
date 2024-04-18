3656fec117616a717f089c06a990225d5746c034
Midomik <krastikrabbs9234@gmail.com>
2024-04-18 19:18:55 +0300

    fix(MF-11): changed path to style.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div>
-      <p>App</p>
+      <p className='bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
+        App
+      </p>
     </div>
   );
 }
