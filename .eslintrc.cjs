89a5373d8816ad8185c802fad414d3178928d42c
Kateryna <ekaterinazuvak1@gmail.com>
2024-04-19 12:31:11 +0300

    fix(MF-9): fixed file .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ const project = resolve(__dirname, 'tsconfig.json');
 
 module.exports = {
   root: true,
-  env: {browser: true, es2020: true},
   extends: [
     require.resolve('@vercel/style-guide/eslint/react'),
     require.resolve('@vercel/style-guide/eslint/typescript'),
