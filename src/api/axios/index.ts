bed33e3565dd32233d95cb1c58fa5faf452b45bc
Kateryna <ekaterinazuvak1@gmail.com>
2024-04-19 16:24:29 +0300

    fix(MF-10): rolled back changes

--- src/api/axios/index.ts
+++ src/api/axios/index.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const BASE_URL: string = import.meta.env.VITE_BASE_URL as string;
 
-export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true,
-});
+export const axiosInstance = axios.create({ baseURL: BASE_URL, withCredentials: true });
+
+
