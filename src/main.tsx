6299e61c156755edc59a68ebcb5b30e73afb91cc
Kateryna <ekaterinazuvak1@gmail.com>
2024-04-30 16:48:46 +0300

    feat(MF-69): added new folders and files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/index.css';
+import './app/styles/index.css';
 import { App } from './App.tsx';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- This rule is disabled due to the fact that we always have such an element.
