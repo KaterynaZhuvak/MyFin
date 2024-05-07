c3708c70bad4f8fdc533f29e79de6c95e8d6b5fe
Midomik <krastikrabbs9234@gmail.com>
2024-05-07 14:55:30 +0300

    fix(MF-24): fixed eslint, renamed page folders, deleted unnecessary dependencies

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import type { FC } from 'react';
 import { App } from './App';
-import { HomePage } from './pages/homePage';
-import { SpendingJournalPage } from './pages/spendingJournalPage';
-import { StatisticsPage } from './pages/statisticsPage';
-import { BudgetingPage } from './pages/budgetingPage';
-import { SettingsPage } from './pages/settingsPage';
-import { ChangeAvatarPage } from './pages/changeAvatarPage';
-import { SignInPage } from './pages/signInPage';
-import { SignUpPage } from './pages/signUpPage';
-import { NotFoundPage } from './pages/notFoundPage';
+import { HomePage } from './pages/HomePage';
+import { SpendingJournalPage } from './pages/SpendingJournalPage';
+import { StatisticsPage } from './pages/StatisticsPage';
+import { BudgetingPage } from './pages/BudgetingPage';
+import { SettingsPage } from './pages/SettingsPage';
+import { ChangeAvatarPage } from './pages/ChangeAvatarPage';
+import { SignInPage } from './pages/SignInPage';
+import { SignUpPage } from './pages/SignUpPage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 export const Router: FC = () => {
   return (
