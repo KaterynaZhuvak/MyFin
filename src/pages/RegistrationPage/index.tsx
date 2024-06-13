5882bd53876e7d0a9aa903cd344d94e70a1c914c
Andy-DoUrden <111894438+Andy-DoUrden@users.noreply.github.com>
2024-06-13 17:23:22 +0300

    chore(MF-106): updated register page ui

--- src/pages/RegistrationPage/index.tsx
+++ src/pages/RegistrationPage/index.tsx
@@ -5,11 +5,17 @@ import { EyeOffIcon } from '../../shared/icons';
 
 interface FormValues {
   username: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
 }
 
 export const RegistrationPage: FC = () => {
   const initialValues: FormValues = {
     username: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
   };
 
   const handleSubmit = (values: FormValues): void => {
@@ -19,56 +25,70 @@ export const RegistrationPage: FC = () => {
   return (
     <div
       className='
+      flex
       h-[1320px]
       w-[full]
+      justify-end
       bg-[url(../../shared/assets/backgrounds/bg-auth.svg),_url(../../shared/assets/backgrounds/bg-auth-lines.svg)]
       bg-[length:1680px_1520px,_1400px_1080px]
       bg-[position:55%_25%,_50%_0]
-      bg-no-repeat'
+      bg-no-repeat 
+      p-[73px_182px_0_0]'
     >
       <div
         className='
-      h-[1184px] 
-      w-[526px] 
-      rounded-[20px] 
-      border
-      bg-black'
+        flex
+        h-[1184px]
+        w-[526px] 
+        justify-center 
+        rounded-[20px] 
+        border
+        bg-black
+        p-[32px_0_72px_0]'
       >
         <Formik initialValues={initialValues} onSubmit={handleSubmit}>
           <Form>
-            Get started now
-            <Input
-              type='password'
-              name='username'
-              placeholder='Enter your username'
-              label='Username'
-              className='h-[64px] w-[378px]'
-            />
-            <Input
-              type='password'
-              name='username'
-              placeholder='Enter your email'
-              label='Your email'
-              className='h-[64px] w-[378px]'
-            />
-            <Input
-              type='password'
-              name='username'
-              placeholder='Enter your password'
-              iconPosition='end'
-              icon={<EyeOffIcon />}
-              label='Password'
-              className='relative h-[64px] w-[378px]'
-            />
-            <Input
-              type='password'
-              name='username'
-              placeholder='Repeat your password'
-              iconPosition='end'
-              icon={<EyeOffIcon />}
-              label='Repeat Password'
-              className='relative h-[64px] w-[378px]'
-            />
+            <h2 className='text-center text-[36px] leading-[54px]'>
+              Get started now
+            </h2>
+
+            <div className='mt-[48px] flex w-[378px] flex-wrap gap-[32px]'>
+              <Input
+                type='username'
+                name='username'
+                placeholder='Enter your username'
+                label='Username'
+                className='h-[64px] w-[378px]'
+              />
+
+              <Input
+                type='email'
+                name='email'
+                placeholder='Enter your email'
+                label='Your email'
+                className='h-[64px] w-[378px]'
+              />
+
+              <Input
+                type='password'
+                name='password'
+                placeholder='Enter your password'
+                iconPosition='end'
+                icon={<EyeOffIcon />}
+                label='Password'
+                className='h-[64px] w-[378px]'
+              />
+
+              <Input
+                type='password'
+                name='repeatPassword'
+                placeholder='Repeat your password'
+                iconPosition='end'
+                icon={<EyeOffIcon />}
+                label='Repeat Password'
+                className='h-[64px] w-[378px]'
+              />
+            </div>
           </Form>
         </Formik>
       </div>
