fe1807d083d8bfabef31485611977c25200a2f7f
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-06-24 12:27:01 +0200

    feat(MF-106): configured registration feature

---
+++ src/features/auth/registaion/RegistraionForm.tsx
@@ -0,0 +1,156 @@
+import { useState, type FC } from 'react';
+import { Form, Formik } from 'formik';
+import { observer } from 'mobx-react';
+import { useNavigate } from 'react-router';
+import * as Yup from 'yup';
+import { Input } from '@shared/ui/Input';
+import { Button } from '@shared/ui/Button';
+import { Icon } from '@shared/icons/Icon';
+import { cookieManager } from '@shared/lib/cookieManager';
+import { useStore } from '@shared/lib/useStore';
+import { registration } from './api/registrationApi';
+import type { RegistrationOptions } from './interfaces/registaion-options';
+
+export const RegistrationForm: FC = observer(() => {
+  const [isVisiblePassword, setIsVisiblePassword] = useState(false);
+  const navigate = useNavigate();
+  const { userStore } = useStore();
+
+  const RegistrationSchema = Yup.object().shape({
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string()
+      .min(8, 'Password must be at least 8 characters')
+      .max(20, 'Password must be at most 20 characters')
+      .required('Required'),
+    repeatPassword: Yup.string()
+      .min(8, 'Password must be at least 8 characters')
+      .max(20, 'Password must be at most 20 characters')
+      .oneOf([Yup.ref('password')], 'Passwords must match')
+      .required('Required'),
+  });
+
+  const initialValues: RegistrationOptions = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  };
+
+  const handleSubmit = async (values: RegistrationOptions): Promise<void> => {
+    if (
+      !values.firstName ||
+      !values.lastName ||
+      !values.email ||
+      !values.password ||
+      !values.repeatPassword
+    ) {
+      return;
+    }
+    const response = await registration(
+      values.firstName,
+      values.lastName,
+      values.email,
+      values.password
+    );
+    userStore.setUserData(response.user);
+    cookieManager.setCookie({
+      name: 'accessToken',
+      value: response.accessToken,
+    });
+    cookieManager.setCookie({
+      name: 'refreshToken',
+      value: response.refreshToken,
+      expires: 7,
+    });
+    navigate('/expenses');
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={RegistrationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className='m-[0_auto]'>
+        <h2 className='text-center text-[36px] leading-[54px]'>
+          Get started now
+        </h2>
+
+        <div className='mt-[48px] flex w-[378px] flex-wrap'>
+          <div className='flex flex-wrap gap-[32px]'>
+            <Input
+              type='firstName'
+              name='firstName'
+              placeholder='Enter your first name'
+              label='First Name'
+              className='h-[64px] w-[378px]'
+            />
+
+            <Input
+              type='lastName'
+              name='lastName'
+              placeholder='Enter your last name'
+              label='Last Name'
+              className='h-[64px] w-[378px]'
+            />
+
+            <Input
+              type='email'
+              name='email'
+              placeholder='Enter your email'
+              label='Your email'
+              className='h-[64px] w-[378px]'
+            />
+
+            <Input
+              type={isVisiblePassword ? 'text' : 'password'}
+              name='password'
+              placeholder='Enter your password'
+              iconPosition='end'
+              icon={
+                <Icon
+                  name={isVisiblePassword ? 'eye' : 'eye-off'}
+                  className='size-[24px]'
+                  onClick={() => {
+                    setIsVisiblePassword(!isVisiblePassword);
+                  }}
+                />
+              }
+              label='Password'
+              className='h-[64px] w-[378px]'
+            />
+
+            <Input
+              type={isVisiblePassword ? 'text' : 'password'}
+              name='repeatPassword'
+              placeholder='Repeat your password'
+              iconPosition='end'
+              icon={
+                <Icon
+                  name={isVisiblePassword ? 'eye' : 'eye-off'}
+                  className='size-[24px]'
+                  onClick={() => {
+                    setIsVisiblePassword(!isVisiblePassword);
+                  }}
+                />
+              }
+              label='Repeat Password'
+              className='h-[64px] w-[378px]'
+            />
+          </div>
+
+          <Button
+            variant='gradient'
+            size='m'
+            title='Register'
+            className='mt-[40px] h-[64px] w-[378px] text-[32px] font-bold'
+            isSubmit
+          />
+        </div>
+      </Form>
+    </Formik>
+  );
+});
--- src/pages/RegistrationPage/index.tsx
+++ src/pages/RegistrationPage/index.tsx
@@ -1,168 +1,36 @@
-import { useState, type FC } from 'react';
-import { Form, Formik } from 'formik';
+import { type FC } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Input } from '../../shared/ui/Input';
-import { Button } from '../../shared/ui/Button';
-import { Icon } from '../../shared/icons/Icon';
-
-interface FormValues {
-  username: string;
-  email: string;
-  password: string;
-  repeatPassword: string;
-}
+import { AlternativeAuthContainer } from '@entities/auth/AlternativeAuthContainer';
+import { AuthBackgroundElement } from '@entities/auth/AuthBackgorundElement/AuthBackgoundElement';
+import { AuthFormContainer } from '@entities/auth/AuthFormContainer';
+import { Divider } from '@entities/auth/Divider/Divider';
+import { AppleAuth } from '@features/auth/by-apple';
+import { GoogleAuth } from '@features/auth/by-google';
+import { RegistrationForm } from '@features/auth/registaion/RegistraionForm';
 
 export const RegistrationPage: FC = () => {
-  const [isVisiblePassword, setIsVisiblePassword] = useState(false);
-
-  const initialValues: FormValues = {
-    username: '',
-    email: '',
-    password: '',
-    repeatPassword: '',
-  };
-
-  const handleSubmit = (values: FormValues): void => {
-    values;
-  };
-
   return (
-    <div
-      className='
-      flex
-      size-full
-      justify-end
-      bg-[url(../../shared/assets/backgrounds/bg-auth.svg),_url(../../shared/assets/backgrounds/bg-auth-lines.svg)]
-      bg-[length:1680px_1520px,_1400px_1080px]
-      bg-[position:50%_10%,_50%_0]
-      bg-repeat
-      p-[73px_182px_64px_0]'
-    >
-      <div
-        className='
-        flex
-        h-auto
-        w-[526px] 
-        justify-center 
-        rounded-[20px] 
-        border
-        bg-black
-        p-[32px_0_72px_0]'
-      >
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-          <Form>
-            <h2 className='text-center text-[36px] leading-[54px]'>
-              Get started now
-            </h2>
-
-            <div className='mt-[48px] flex w-[378px] flex-wrap'>
-              <div className='flex flex-wrap gap-[32px]'>
-                <Input
-                  type='username'
-                  name='username'
-                  placeholder='Enter your username'
-                  label='Username'
-                  className='h-[64px] w-[378px]'
-                />
-
-                <Input
-                  type='email'
-                  name='email'
-                  placeholder='Enter your email'
-                  label='Your email'
-                  className='h-[64px] w-[378px]'
-                />
-
-                <Input
-                  type={isVisiblePassword ? 'text' : 'password'}
-                  name='password'
-                  placeholder='Enter your password'
-                  iconPosition='end'
-                  icon={
-                    <Icon
-                      name={isVisiblePassword ? 'eye' : 'eye-off'}
-                      className='size-[24px]'
-                      onClick={() => {
-                        setIsVisiblePassword(!isVisiblePassword);
-                      }}
-                    />
-                  }
-                  label='Password'
-                  className='h-[64px] w-[378px]'
-                />
-
-                <Input
-                  type={isVisiblePassword ? 'text' : 'password'}
-                  name='repeatPassword'
-                  placeholder='Repeat your password'
-                  iconPosition='end'
-                  icon={
-                    <Icon
-                      name={isVisiblePassword ? 'eye' : 'eye-off'}
-                      className='size-[24px]'
-                      onClick={() => {
-                        setIsVisiblePassword(!isVisiblePassword);
-                      }}
-                    />
-                  }
-                  label='Repeat Password'
-                  className='h-[64px] w-[378px]'
-                />
-              </div>
-
-              <Button
-                variant='gradient'
-                size='m'
-                title='Register'
-                className='mt-[40px] h-[64px] w-[378px] text-[32px] font-bold'
-              />
-
-              <div className='mt-[40px] flex h-[41px] w-[378px] items-center justify-center gap-[12px]'>
-                <div className='w-[165px] border-t border-white' />
-                <span className='text-[20px]'>or</span>
-                <div className='w-[165px] border-t border-white' />
-              </div>
-
-              <div className='mt-[40px] flex flex-wrap gap-[32px]'>
-                <Button
-                  disabled
-                  variant='outline'
-                  size='m'
-                  title='Continue with Google'
-                  className='flex h-[64px] w-[372px] items-center justify-center gap-[16px] text-[20px]'
-                  iconPosition='start'
-                  icon={<Icon name='google' className='size-[24px]' />}
-                />
-
-                <Button
-                  disabled
-                  variant='outline'
-                  size='m'
-                  title='Continue with Apple'
-                  className='flex h-[64px] w-[372px] items-center justify-center gap-[16px] text-[20px]'
-                  iconPosition='start'
-                  icon={<Icon name='apple' className='h-[28px] w-[24px]' />}
-                />
-
-                <span className='w-full text-center'>
-                  Already a member?{' '}
-                  <NavLink to='#' className='text-[#27DAB1] underline'>
-                    Log In
-                  </NavLink>
-                </span>
-
-                <span className='w-full text-center'>
-                  By registering you agree to the{' '}
-                  <NavLink to='#' className='text-[#27DAB1] underline'>
-                    terms of service
-                  </NavLink>
-                  .
-                </span>
-              </div>
-            </div>
-          </Form>
-        </Formik>
-      </div>
-    </div>
+    <AuthBackgroundElement>
+      <AuthFormContainer>
+        <RegistrationForm />
+        <Divider />
+        <AlternativeAuthContainer>
+          <GoogleAuth />
+          <AppleAuth />
+        </AlternativeAuthContainer>
+        <span className='mt-[24px] w-full text-center'>
+          Already a member?{' '}
+          <NavLink to='/login' className='text-[#27DAB1] underline'>
+            Log In
+          </NavLink>
+        </span>
+        <span className='mt-[24px] w-full text-center'>
+          By registering you agree to the{' '}
+          <NavLink to='/terms-of-service' className='text-[#27DAB1] underline'>
+            Terms of Service
+          </NavLink>
+        </span>
+      </AuthFormContainer>
+    </AuthBackgroundElement>
   );
 };
