f699b0622bb983a7e47291b3c34f5af5547bcdc4
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-06-25 18:19:19 +0200

    fix(MF-105): fixed wrong interpretation of interceptors

--- src/features/PrivateRoute/index.tsx
+++ src/features/PrivateRoute/index.tsx
@@ -1,9 +1,13 @@
 import type { FC } from 'react';
 import { Navigate, Outlet } from 'react-router';
 import { cookieManager } from '@shared/lib/cookieManager';
+import { setupAuthInterceptors } from '@features/auth/model/auth.interceptor';
+import { axiosInstance } from '@shared/api/axios';
 
 export const PrivateRoute: FC = () => {
   const token = cookieManager.getCookie('accessToken');
+
+  setupAuthInterceptors(axiosInstance);
 
   return token ? <Outlet /> : <Navigate to='/login' />;
 };
--- src/features/auth/model/auth.interceptor.ts
+++ src/features/auth/model/auth.interceptor.ts
@@ -1,44 +1,40 @@
+import type { AxiosInstance } from 'axios';
 import { cookieManager } from '@shared/lib/cookieManager.ts';
 import { axiosInstance } from '@shared/api/axios';
 import { refresh } from '../refresh/refresh.api';
+import { setCookies } from './setCookies';
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = cookieManager.getCookie('accessToken');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+export const setupAuthInterceptors = (Api: AxiosInstance): void => {
+  Api.interceptors.request.use(
+    (config) => {
+      const token = cookieManager.getCookie('accessToken');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(new Error(error as string));
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(new Error(error as string));
-  }
-);
-
-axiosInstance.interceptors.response.use(
-  async (response) => {
-    const accessToken = cookieManager.getCookie('accessToken');
-    const refreshToken = cookieManager.getCookie('refreshToken');
-    if (response.status === 403) {
-      await refresh(accessToken, refreshToken)
-        .then((data) => {
-          cookieManager.setCookie({
-            name: 'accessToken',
-            value: data.accessToken,
+  );
+  Api.interceptors.response.use(
+    async (response) => {
+      const accessToken = cookieManager.getCookie('accessToken');
+      const refreshToken = cookieManager.getCookie('refreshToken');
+      if (response.status === 401) {
+        await refresh(accessToken, refreshToken)
+          .then((data) => {
+            setCookies(data.accessToken, data.refreshToken, 7);
+            return axiosInstance.request(response.config);
+          })
+          .catch((error: unknown) => {
+            return Promise.reject(new Error(error as string));
           });
-          cookieManager.setCookie({
-            name: 'refreshToken',
-            value: data.refreshToken,
-          });
-          return axiosInstance.request(response.config);
-        })
-        .catch((error: unknown) => {
-          return Promise.reject(new Error(error as string));
-        });
+      }
+      return response;
+    },
+    (error) => {
+      return Promise.reject(new Error(error as string));
     }
-    return response;
-  },
-  (error) => {
-    return Promise.reject(new Error(error as string));
-  }
-);
+  );
+};
