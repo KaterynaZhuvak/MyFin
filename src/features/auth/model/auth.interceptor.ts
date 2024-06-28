c78d78053f5a72d446cd358dbe35465c5c95a85f
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-06-28 17:06:49 +0200

    fix(MF-105): fixed interceptors

--- src/features/auth/model/auth.interceptor.ts
+++ src/features/auth/model/auth.interceptor.ts
@@ -1,8 +1,7 @@
-import type { AxiosInstance } from 'axios';
+import axios from 'axios';
+import type { AxiosError, AxiosInstance } from 'axios';
 import { cookieManager } from '@shared/lib/cookieManager.ts';
-import { axiosInstance } from '@shared/api/axios';
 import { refresh } from '../refresh/refresh.api';
-import { setCookies } from './setCookies';
 
 export const setupAuthInterceptors = (Api: AxiosInstance): void => {
   Api.interceptors.request.use(
@@ -17,24 +16,38 @@ export const setupAuthInterceptors = (Api: AxiosInstance): void => {
       return Promise.reject(new Error(error as string));
     }
   );
+
   Api.interceptors.response.use(
-    async (response) => {
-      const accessToken = cookieManager.getCookie('accessToken');
-      const refreshToken = cookieManager.getCookie('refreshToken');
-      if (response.status === 401) {
-        await refresh(accessToken, refreshToken)
-          .then((data) => {
-            setCookies(data.accessToken, data.refreshToken, 7);
-            return axiosInstance.request(response.config);
-          })
-          .catch((error: unknown) => {
-            return Promise.reject(new Error(error as string));
-          });
-      }
+    (response) => {
       return response;
     },
-    (error) => {
-      return Promise.reject(new Error(error as string));
+    async (error: AxiosError) => {
+      const originalRequest = error.config;
+      if (error.response?.status === 401) {
+        try {
+          const data = await refresh(
+            cookieManager.getCookie('accessToken'),
+            cookieManager.getCookie('refreshToken')
+          );
+          cookieManager.setCookie({
+            name: 'accessToken',
+            value: data.accessToken,
+          });
+          originalRequest
+            ? (originalRequest.headers.Authorization = `Bearer ${data.accessToken}`)
+            : null;
+          return axios(window.location.pathname, originalRequest);
+        } catch (error2) {
+          const errorValue = error2 as AxiosError;
+          if (errorValue.response?.status === 403) {
+            cookieManager.removeCookie('accessToken');
+            cookieManager.removeCookie('refreshToken');
+            window.location.href = '/login';
+          }
+          return Promise.reject(new Error(error2 as string));
+        }
+      }
+      return Promise.reject(error);
     }
   );
 };
