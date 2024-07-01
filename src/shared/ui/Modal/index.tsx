28d4f5050a620a7b836c0a120569457210a364fb
Kateryna <ekaterinazuvak1@gmail.com>
2024-07-01 21:21:52 +0200

    feat(MF-30): deleted useEffect from modal and replace it

--- src/shared/ui/Modal/index.tsx
+++ src/shared/ui/Modal/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, type FC, type ReactNode } from 'react';
+import { type FC, type ReactNode } from 'react';
 import Modal from 'react-modal';
 
 interface ModalProps {
@@ -14,23 +14,13 @@ export const CustomModal: FC<ModalProps> = ({
   isOpen,
   onRequestClose,
 }) => {
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = '';
-    }
-    return () => {
-      document.body.style.overflow = '';
-    };
-  }, [isOpen]);
-
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       overlayClassName='bg-black/50 fixed top-0 left-0 right-0 bottom-0 '
       className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ${className}`}
+      bodyOpenClassName='overflow-hidden'
       shouldCloseOnOverlayClick
       shouldCloseOnEsc
     >
