537268cb3e11aa00c08c4378df63481a02b95bc6
Kateryna <ekaterinazuvak1@gmail.com>
2024-07-02 12:57:09 +0200

    feat(MF-30): added cn to modal

--- src/shared/ui/Modal/index.tsx
+++ src/shared/ui/Modal/index.tsx
@@ -1,5 +1,6 @@
 import { type FC, type ReactNode } from 'react';
 import Modal from 'react-modal';
+import { cn } from '@shared/lib/cn';
 
 interface ModalProps {
   className: string;
@@ -19,7 +20,10 @@ export const CustomModal: FC<ModalProps> = ({
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       overlayClassName='bg-black/50 fixed top-0 left-0 right-0 bottom-0 '
-      className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ${className}`}
+      className={cn(
+        'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2',
+        className
+      )}
       bodyOpenClassName='overflow-hidden'
       shouldCloseOnOverlayClick
       shouldCloseOnEsc
