618fe8494a4ec65fa506886c75f6fff2fd675013
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-07-09 14:21:45 +0200

    refactor(MF-119): finished refactoring of the icons for the input

--- src/shared/ui/Input/index.tsx
+++ src/shared/ui/Input/index.tsx
@@ -1,6 +1,7 @@
 import { type ReactNode, type FC, type InputHTMLAttributes } from 'react';
-import { Field, ErrorMessage } from 'formik';
+import { Field, ErrorMessage, useField } from 'formik';
 import { type VariantProps, cva } from 'class-variance-authority';
+import { Icon } from '@shared/icons/Icon';
 import { cn } from '../../lib/cn';
 
 const inputVariants = cva(
@@ -10,6 +11,9 @@ const inputVariants = cva(
       FormInput: {
         authInput:
           'h-[44px] w-[318px] rounded-[10px] pl-5 text-base tablet:h-[64px] tablet:w-[378px] tablet:rounded-[15px]',
+      },
+      NotFormInput: {
+        formOk: '',
       },
     },
   }
@@ -31,6 +35,7 @@ export const Input: FC<InputProps> = ({
   FormInput,
   ...rest
 }) => {
+  const [_, meta] = useField(name);
   return (
     <label
       className='relative flex w-full flex-col gap-3 font-sans text-xl text-white'
@@ -45,6 +50,18 @@ export const Input: FC<InputProps> = ({
           {...rest}
         />
         {icon}
+        {meta.touched && !meta.error && FormInput === 'authInput' ? (
+          <Icon
+            name='form-ok'
+            className={`absolute ${icon ? 'right-[50px]' : 'right-[16px]'} top-1/2 size-[20px] -translate-y-1/2 rounded-full bg-gradient-to-br from-[#05A67B] via-[#093423] to-[#093423] tablet:right-[-40px] tablet:size-[24px]`}
+          />
+        ) : null}
+        {meta.touched && meta.error && FormInput === 'authInput' ? (
+          <Icon
+            name='form-error'
+            className={`absolute ${icon ? 'right-[50px]' : 'right-[16px]'} top-1/2 size-[20px] -translate-y-1/2 rounded-full bg-gradient-to-t from-[#FF4444] to-[#FF7363] tablet:right-[-40px] tablet:size-[24px]`}
+          />
+        ) : null}
       </div>
       <ErrorMessage name={name}>
         {(msg) => <div className='text-red-500'>{msg}</div>}
