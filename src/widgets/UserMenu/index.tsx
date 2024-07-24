09fa17285d1d3c062a5a9473a72812e22a95c64e
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-07-24 20:14:57 +0200

    chore(MF-123): configured styles for the arrow

--- src/widgets/UserMenu/index.tsx
+++ src/widgets/UserMenu/index.tsx
@@ -12,14 +12,14 @@ export const UserMenu: FC = () => {
   return (
     <PopoverComponent
       trigger={
-        <div className='relative order-4 flex h-[56px] cursor-pointer flex-row items-center justify-center self-center justify-self-center tablet:justify-self-start'>
+        <div className='user-popover relative order-4 flex h-[56px] cursor-pointer flex-row items-center justify-center self-center justify-self-center tablet:justify-self-start'>
           <AvatarComponent firstName={user?.firstName} />
           <span className='ml-[16px] hidden h-[24px] w-full select-none text-nowrap text-[20px] desktop:block'>
             {user?.firstName} {user?.lastName[0]}.
           </span>
           <Icon
             name='arrow'
-            className='absolute bottom-[9px] right-[-32px] hidden size-[32px] desktop:block'
+            className='user-popover-arrow absolute bottom-[9px] right-[-32px] hidden size-[32px] desktop:block'
           />
         </div>
       }
