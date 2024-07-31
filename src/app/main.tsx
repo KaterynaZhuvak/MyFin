677620fb8c3db2df5ae85b88134eb31a4efe4d29
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-07-31 20:45:41 +0200

    chore(MF-142): configured react-query

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import ReactModal from 'react-modal';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClientProvider } from '@tanstack/react-query';
 import './styles/index.css';
+import { queryClient } from '@shared/api/query-client';
 import { App } from './App';
 import { StoreProvider } from './providers/storeProvider';
 
@@ -10,8 +13,11 @@ ReactModal.setAppElement('#root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- This rule is disabled due to the fact that we always have such an element.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StoreProvider>
-      <App />
-    </StoreProvider>
+    <QueryClientProvider client={queryClient}>
+      <StoreProvider>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </StoreProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
